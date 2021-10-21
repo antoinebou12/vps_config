dbe3e70ced5b332cf287c234b93928d2d5fee47b
Antoine Boucher <antoine.bou13@gmail.com>
2021-10-20 22:28:51 -0400

    Update ansible.sh

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -6,5 +6,6 @@ install_ansible() {
 
 install_ansible_galaxy () {
 	ansible-galaxy install geerlingguy.docker
+	ansible-galaxy install geerlingguy.pip
 	ansible-galaxy collection install maxhoesel.caddy
 }
